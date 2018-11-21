03ac8e6037164662dca53d829acc73441c2aee7c
Lukas Fam <takeruadelbert@gmail.com>
2018-11-21 13:12:38 +0700

    prevebnt double submission form

--- app/webroot/js/functions.js
+++ app/webroot/js/functions.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
         autolist(this);
     });
     loadckeditor();
+    $('form').preventDoubleSubmission();
 })
 
 function doFilter(e) {
@@ -463,3 +464,19 @@ function autolist(e) {
         }
     })
 }
+
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function () {
+    $(this).on('submit', function (e) {
+        var $form = $(this);
+        if ($form.data('submitted') === true) {
+            // Previously submitted - don't submit again
+            e.preventDefault();
+        } else {
+            // Mark it so that the next submit can be ignored
+            $form.data('submitted', true);
+        }
+    });
+    // Keep chainability
+    return this;
+};
